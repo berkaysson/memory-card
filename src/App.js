11931c99c46b2624a43f20789d0f5bc8d712210a
berkaysson <berkaysonel85@gmail.com>
2023-04-01 15:19:19 +0300

    Update README.md

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Game from './Components/Game';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Game />
     </div>
   );
 }

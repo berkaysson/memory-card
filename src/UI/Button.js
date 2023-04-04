c7f5c47d28c91f1da93dd13e79c3eab6679ba648
berkaysson <berkaysonel85@gmail.com>
2023-04-04 22:03:48 +0300

    Stylize Button component

--- src/UI/Button.js
+++ src/UI/Button.js
@@ -8,12 +8,22 @@ const Button = ({onClick,text}) => {
 export default Button;
 
 const ButtonWrapper = styled.button`
-  padding: 10px 20px;
-  font-size: 16px;
-  background-color:  var(--dark-color);
+  text-shadow: rgba(0, 0, 0, 0.25) 0 3px 8px;
+  padding: 1.5rem 3rem;
+  font-size: var(--font-lg);
+  background:linear-gradient(90deg, var(--ls-blue) 0%, var(--ls-purple) 100%);
   color: var(--light-color);
   border: none;
   border-radius:var(--button-border-radius);
   cursor: pointer;
-  animation: ${glowEffect('blue', 'box')} 2s linear infinite;
+  transition:all 0.3s ease-in-out ;
+
+  &:hover{
+    transform: scale(1.02);
+    animation: ${glowEffect('blue', 'box')} 2s linear infinite;
+  }
+
+  &:active{
+    transform: scale(1);
+  }
 `

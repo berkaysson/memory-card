09437aae245b1b0cf4f5fc500618cec2c765138f
berkaysson <berkaysonel85@gmail.com>
2023-04-04 20:35:35 +0300

    Style Header.js, add glow effect

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,16 @@
+import styled from "styled-components";
+import glowEffect from "../Utils/glowEffect";
+
 const Header = () => {
   return <header>
-    <h1>Memory Card Game</h1>
+    <H1Wrapper>Memory Card Game</H1Wrapper>
   </header>
 }
+
+const H1Wrapper = styled.h1`
+  font-size: var(--font-xlg);
+  text-shadow: 0px 0px 10px rgba(252, 163, 17, 1);
+  animation: ${glowEffect('purple','text')} 2s linear infinite;
+`
 
 export default Header;
